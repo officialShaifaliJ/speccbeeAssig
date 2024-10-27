907b0077d80b7c9495664e735b6a4d6c0c98b85f
shaifali <shaifali02000@gmail.com>
2024-10-27 12:28:13 +0530

    speccbee-new2

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 const speakerCard = document.getElementById('card'); 
-const speakerModal = document.getElementById('modal');
-const modalClose = document.getElementById('close');
+const speakerCard2 = document.getElementById('card2'); 
+const speakerCard3 = document.getElementById('card3'); 
 
+
+const speakerModal = document.getElementById('modal');
+const speakerModal2 = document.getElementById('modal2');
+const speakerModal3 = document.getElementById('modal3');
+
+
+const modalClose = document.getElementById('close');
+const modalClose2 = document.getElementById('close2');
+const modalClose3 = document.getElementById('close3');
+
+// modal1
 speakerCard.addEventListener('click', () => {
   speakerModal.style.display = 'block';
 });
@@ -9,4 +20,24 @@ speakerCard.addEventListener('click', () => {
 modalClose.addEventListener('click', () => {
   speakerModal.style.display = 'none'; 
 });
+
+// modal2
+
+speakerCard2.addEventListener('click', () => {
+    speakerModal2.style.display = 'block';
+  });
+  
+  modalClose2.addEventListener('click', () => {
+    speakerModal2.style.display = 'none'; 
+  });
+
+// modal3
+
+  speakerCard3.addEventListener('click', () => {
+    speakerModal3.style.display = 'block';
+  });
+  
+  modalClose3.addEventListener('click', () => {
+    speakerModal3.style.display = 'none'; 
+  });
 
